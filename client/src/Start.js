f69b8eec67931b6ce7bfb5c05d52baeb9218b17e
cblacks26 <wcblackstock@gmail.com>
2019-05-02 22:17:10 -0600

    Added homepage ui

--- client/src/Start.js
+++ client/src/Start.js
@@ -1,59 +1,28 @@
 import React, { Component } from "react";
+import { Button } from 'reactstrap';
 class Start extends Component {
     constructor(props){
         super(props);
         this.state = {
             accounts: this.props.accounts,
-            contract: this.props.contract,
-            games: null
+            contract: this.props.contract
         };
-        this.findGames = this.findGames.bind(this);
-        this.renderGames = this.renderGames.bind(this);
-        this.updateGame = this.updateGame.bind(this);
         this.createGame = this.createGame.bind(this);
-    }
-
-    async findGames(){
-        try{
-            var games = await this.props.contract.methods.getJoinableGames().call({
-                from: this.props.accounts[0]
-            });
-            this.setState({ 
-                games: games
-            });
-        }catch (err) {
-            this.setState({ errorMessage: err.message });
-        }
-    }
-
-    updateGame(event){
-        this.props.updateGame(event.target.value);
     }
 
     createGame(){
         this.props.createGame();
     }
 
-    renderGames(){
-        let games = [];
-        for(let game in this.state.games){
-            games.push(<li><button onClick={this.updateGame} value={game}>{game}</button></li>)
-        }
-        return games;
-    }
-
     render(){
         return(
-            <div className="welcome">
-                <div className="col">
-                    <ul>
-                        {this.renderGames()}
-                    </ul>
-                </div>
-                <div className="col">
-                    <button onClick={this.createGame}>Create Game</button>
-                </div>
-            </div>
+            <Jumbotron fluid>
+                <Container fluid className="center">
+                    <h1 className="display-2">Sudoku</h1>
+                    <p className="lead">This is a straight forward sudoku game built using smart contracts, so expect almost everything to be a little slower</p>
+                    <Button outline color="primary" onClick={this.createGame}>Create Game</Button>
+                </Container>
+            </Jumbotron> 
         );
     }
 }export default Start;
